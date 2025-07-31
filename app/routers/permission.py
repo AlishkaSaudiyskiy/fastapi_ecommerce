3b537240612ad9dc5ca308bb6f905b53739ccb24
Aliy Djanibekov <aliy.janibekov.777@mail.ru>
2025-07-31 11:26:04 +0300

    Update file app/routers/permission.py. Implementation of functionality for changing the activity status of users by the administrator and deleting users by the administrator

--- app/routers/permission.py
+++ app/routers/permission.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 
-from sqlalchemy import select, update
+from sqlalchemy import select, update, delete
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -19,7 +19,8 @@ router = APIRouter(prefix='/permission', tags=['permission'])
 async def supplier_permission(
     db: Annotated[AsyncSession, Depends(get_db)], 
     get_user: Annotated[dict, Depends(get_current_user)],
-    user_id: int):
+    user_id: int
+):
     if get_user.get('is_admin'):
         user = await db.scalar(select(User).where(User.id == user_id))
 
@@ -47,3 +48,71 @@ async def supplier_permission(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="You don't have admin permission"
         )
+    
+
+@router.patch('/change/active')
+async def is_active_change(
+    db: Annotated[AsyncSession, Depends(get_db)], 
+    get_user: Annotated[dict, Depends(get_current_user)],
+    user_id: int
+):
+    if get_user.get('is_admin'):
+        user = await db.scalar(select(User).where(User.id == user_id))
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User not found'
+            )
+        if user.is_admin:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You can't deactivate yourself"
+            )
+        if user.is_active:
+            await db.execute(update(User).where(User.id == user_id).values(is_active=False))
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User is deactivate'
+            }
+        else:
+            await db.execute(update(User).where(User.id == user_id).values(is_active=True))
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User is activated'
+            }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You don't have admin permission"
+        )
+    
+
+@router.delete('/delete')
+async def delete_user(
+    db: Annotated[AsyncSession, Depends(get_db)], 
+    get_user: Annotated[dict, Depends(get_current_user)],
+    user_id: int
+):
+    if get_user.get('is_admin'):
+        user = await db.scalar(select(User).where(User.id == user_id))
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User not found'
+            )
+        
+        await db.execute(delete(User).where(User.id == user_id))
+        await db.commit()
+        return {
+                'status_code': status.HTTP_200_OK,
+                'detail': f'User {user_id} deleted'
+            } 
+    
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You don't have admin permission"
+        ) 
