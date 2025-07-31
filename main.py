90e201d2dfbe6cf2c536e80548980c54873f6a3b
Aliy Djanibekov <aliy.janibekov.777@mail.ru>
2025-07-31 10:54:45 +0300

    Create new file app/routers/permission.py and Update file main.py. Implementation of user status changes by the admin

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 from app.routers.category import router as router_category
 from app.routers.products import router as router_products
 from app.routers.auth import router as router_auth
+from app.routers.permission import router as router_permission
 
 app = FastAPI()
 app.include_router(router_category)
 app.include_router(router_products)
 app.include_router(router_auth)
+app.include_router(router_permission)
 
 
 @app.get("/")
